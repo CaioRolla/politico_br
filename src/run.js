e7e5653ea5a3934f1d84174775b4046ab5ad85bf
Caio Rolla <caiioocesar@outlook.com>
2019-03-25 18:58:43 -0300

    :hammer: Making alg better

--- src/run.js
+++ src/run.js
@@ -1,7 +1,7 @@
 const TwitterService = require('./services/twitter');
 const Markov = require('markov-strings').default;
 
-(async function run(stateSize) {
+(async function run(stateSize = 3) {
   try {
     // Get every "following". Let's call it "friends".
     const friendsResponse = await TwitterService.getFriends();
@@ -20,31 +20,36 @@ const Markov = require('markov-strings').default;
     const tweetsText = tweetsResponse.statuses.map(tweet => tweet.text);
 
     const cleanedTweets = tweetsText.map(tweet => {
-      return tweet
-        .replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')
-        .replace(
+      return tweet.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+      /*.replace(
           /([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g,
           ''
-        );
+        );*/
     });
 
-    const filteredTweets = cleanedTweets.filter(tweet => tweet.length >= 100);
+    const filteredTweets = cleanedTweets.filter(tweet => tweet.length >= 50);
 
     const options = {
       maxTries: 99999,
       filter: result => {
-        return result.string.split(' ').length >= 3;
+        return (
+          result.string.split(' ').length >= 3 &&
+          result.string.split(' ').length < 250 &&
+          !result.string.endsWith('…')  
+        );
       }
     };
 
     const markov = new Markov(filteredTweets, { stateSize });
     markov.buildCorpus();
     const result = markov.generate(options);
-    console.log(result.string);
+    console.log(result.tries, result.string.length, result.string);
   } catch (error) {
-    if (stateSize < 5) {
-      run(stateSize + 1);
+    if (stateSize - 1 > 0) {
+      run(stateSize - 1);
     }
     console.log(error);
   }
-})(1);
+})(3);
+
+
