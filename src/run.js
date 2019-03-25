81077ca9c2a9e2316d5b986f48b2fb622d3df873
Caio Rolla <caiioocesar@outlook.com>
2019-03-25 17:38:29 -0300

    :sparkles: Basic alg to retrieve tweets and clean then

--- src/run.js
+++ src/run.js
@@ -1,1 +1,21 @@
 const TwitterService = require('./services/twitter');
+
+(async () => {
+  const friendsResponse = await TwitterService.getFriends();
+
+  const friendsScreenNames = friendsResponse.users.map(friend => {
+    return friend.screen_name;
+  });
+
+  const tweetsResponse = await TwitterService.getTweetsFromScreenNames(friendsScreenNames);
+
+  const tweetsText = tweetsResponse.statuses.map((tweet)=>{
+    return tweet.text;
+  });
+
+  const cleanedTweets = tweetsText.map((tweet) => {
+    return tweet.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+  });
+
+  console.log(cleanedTweets);
+})();
